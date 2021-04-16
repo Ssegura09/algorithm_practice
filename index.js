eafff01c9432f890c8b79112125630e9b0c73468
Ssegura09 <stephaniemsegura09@gmail.com>
2021-04-15 19:41:34 -0500

    .

---
+++ index.js
@@ -0,0 +1,8 @@
+// let array = ["a","b","c","a","a","a","b","b","b"]
+
+function test(arr){
+    for(let i = 0; i < arr.length; i++){
+        console.log(arr[i])
+    }
+}
+test(["a","b","c","a","a","a","b","b","b"])
